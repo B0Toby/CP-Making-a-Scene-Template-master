77abac54fe420ba9841dc8ea8f0cddcf29e06a34
Toby Pang <xupang@ucsc.edu>
2024-01-19 19:47:37 -0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,15 @@
+// Code Practice: Making a Scene
+// Name: Toby Pang
+// Date: 01/17/2024
+
+"use strict"
+
+let config = {
+    type: Phaser.AUTO,
+    render: {
+        pixelArt: true
+    },
+    scene: [ MainMenu, Play ]
+}
+
+let game = new Phaser.Game(config)
